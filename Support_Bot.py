6a1fdad3f5228577f02d9a3d563208923f89a26d
Marshak2k7 <ustim2k7@gmail.com>
2021-07-15 13:55:52 +0300

    Version 0.2

--- Support_Bot.py
+++ Support_Bot.py
@@ -4,13 +4,19 @@
 - Авторизация по FreshDesk api
 - Создание тикета через ввод (клике) команд
 - Пропуск почты, замена на telegram@visiology.su
+- Описание тикета принимает несколько сообщений от пользователя
 """
-
 
 import telebot
 
 from freshdesk.api import API
 import datetime as dt
+
+
+def start_ticket_info_collection(chat_id):
+    chat_id_started_collection[chat_id] = True
+    chat_id_ticket_collection[chat_id] = []
+
 
 today_date = dt.datetime.today().strftime('%Y.%m.%d %H:%M')
 
@@ -21,6 +27,9 @@ bot = telebot.TeleBot(access_token)
 user_dict = {}
 ticket_dict = {}
 
+chat_id_started_collection = {}
+chat_id_ticket_collection = {}
+
 
 class Ticket:
     def __init__(self, email):
@@ -28,6 +37,13 @@ class Ticket:
         self.tags = ['Telegram']
         self.email = email
         self.description = None
+
+    def create_ticket(self, api):
+        ticket = api.tickets.create_ticket(self.subject,
+                                           email=self.email,
+                                           description=self.description,
+                                           tags=self.tags)
+        return ticket
 
 
 class User:
@@ -55,12 +71,12 @@ def get_api(message):
         a.tickets.list_tickets()
 
         user.is_auth = True
-        bot.send_message(message.from_user.id,
+        bot.send_message(chat_id,
                          'Авторизация прошла успешно, введите команду /create, чтобы создать тикет')
     except:
 
         user.is_auth = False
-        bot.send_message(message.from_user.id, 'Неправильно, введите api заново')
+        bot.send_message(chat_id, 'Неправильно, введите api заново')
         bot.register_next_step_handler(message, get_api)
 
 
@@ -71,12 +87,13 @@ def ticket_create(message):
         user = user_dict[chat_id]
         if message.text == '/create':
             if user.is_auth:
-                bot.send_message(message.from_user.id, 'Введите почту клиента')
+                bot.send_message(chat_id, 'Введите почту клиента или введите команду /skip, чтобы '
+                                          'пропустить')
                 bot.register_next_step_handler(message, get_email)
             else:
-                bot.send_message(message.from_user.id, 'Сначала авторизуйтесь с помощью команды /start')
+                bot.send_message(chat_id, 'Сначала авторизуйтесь с помощью команды /start')
     except:
-        bot.send_message(message.from_user.id, 'Сначала авторизуйтесь с помощью команды /start')
+        bot.send_message(chat_id, 'Сначала авторизуйтесь с помощью команды /start')
 
 
 def get_email(message):
@@ -90,29 +107,53 @@ def get_email(message):
         ticket = Ticket(email)
         ticket_dict[chat_id] = ticket
 
-        bot.send_message(message.from_user.id, 'Введите описание тикета')
-        bot.register_next_step_handler(message, get_description)
+        bot.send_message(message.from_user.id, 'Введите описание тикета, а затем выполните команду /stop')
+
+        # bot.register_next_step_handler(message, get_description)
+        start_ticket_info_collection(chat_id)
     except:
         bot.reply_to(message, 'Почта введена неверно')
 
 
-def get_description(message):
+@bot.message_handler(content_types=["text", "photo", "file"])
+def process_message(message):
     chat_id = message.chat.id
-    description = message.text
+    if message.text == '/stop':
+        stop_collection(message)
+
+    elif chat_id_started_collection.get(chat_id, False):
+        description = message.text
+        chat_id_ticket_collection[chat_id].append(description)
+    else:
+        bot.send_message(chat_id, 'Вы кто такие?')
+
+
+def save_ticket_info(chat_id, ticket_info_text):
+    bot.send_message(chat_id, ticket_info_text)
 
     user = user_dict[chat_id]
     ticket = ticket_dict[chat_id]
 
-    ticket.description = description
+    ticket.description = ticket_info_text
     try:
         a = API('newaccount1624950159366.freshdesk.com', user.api)
-        create_ticket = a.tickets.create_ticket(ticket.subject,
-                                                email=ticket.email,
-                                                description=ticket.description,
-                                                tags=ticket.tags)
-        bot.send_message(message.from_user.id, 'Тикет создан удачно')
+        _ = ticket.create_ticket(a)
+        bot.send_message(chat_id, 'Тикет создан удачно')
     except Exception:
-        bot.send_message(message.from_user.id, 'Упс, что-то пошло не так. Попробуйте создать тикет заново: /create')
+        bot.send_message(chat_id, 'Упс, что-то пошло не так. Попробуйте создать тикет заново: /create')
+
+
+@bot.message_handler(commands=['stop'])
+def stop_collection(message):
+    chat_id = message.chat.id
+    chat_id_started_collection[chat_id] = False
+
+    ticket_info_list = chat_id_ticket_collection.get(chat_id, []).copy()
+    chat_id_ticket_collection[chat_id] = []
+
+    ticket_info_text = '<br><br>'.join(ticket_info_list)
+    if ticket_info_text:
+        save_ticket_info(chat_id, ticket_info_text)
 
 
 bot.polling(none_stop=True)
